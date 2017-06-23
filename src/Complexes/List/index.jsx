ed05bf44ba001de8d22511ec7cef7fe0668907dc
AnnEagle <mail2anuta.ru@gmail.com>
2017-06-23 23:56:05 +0300

    fix List/index

--- src/Complexes/List/index.jsx
+++ src/Complexes/List/index.jsx
@@ -21,36 +21,30 @@ export default () => {
             name="SOUTH BEACH, SAN FRANCISCO"
             address="764 Metropolitan Avenue"
           >
-            <div>
               The Lewis Steel Building is a masterful industrial conversion
               located in the heart of Williamsburg.
               Located at 76 North 4th Street, the former 1930's steel factory
               has been transformed into 83 individually unique and luxury loft
               apartments.
-            </div>
           </Card>
           <Card
             id={2}
             name="MIDTOWN EAST, MANHATTAN"
             address="100 East 53rd Street"
           >
-            <div>
               One Hundred East Fifty Third Street by Foster + Partners is a
               limited collection of modern residences in Midtown Manhattan's
               Cultural District.
               The 94 residences ranging from alcove lofts to four bedrooms
               within the 63-story tower are generously proportioned.
-            </div>
           </Card>
           <Card id={3} name="NOLITA, MANHATTAN" address="152 Elizabeth">
-            <div>
               152 Elizabeth is an ultra-luxury condominium building—the first in
               New York City designed by Japanese master architect Tadao Ando.
               Located at the corner of Kenmare and Elizabeth Streets in Nolita,
               the 32,000-square-foot building will stand as a profound
               architectural statement and embrace the industrial character of
               the neighborhood.
-            </div>
           </Card>
         </Grid>
       </Article>
